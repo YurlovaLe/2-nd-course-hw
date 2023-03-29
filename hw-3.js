fd3bdc49bac4b0decc909755748f07a87a9c23de
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-03-29 11:53:27 +0700

    task4

--- hw-3.js
+++ hw-3.js
@@ -7,3 +7,45 @@ if (enteredPassword===password) {
   console.log('Пароль введен неправильно');
 }
 
+//Задание 2
+let c = 0;
+if (c>0 && c<10) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+c = 10;
+if (c>0 && c<10) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+c = -3;
+if (c>0 && c<10) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+c = 2;
+if (c>0 && c<10) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+//Задание 3
+let d = 15;
+let e = 110;
+if (d>100 || e>100) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+//Задание 4
+let a = '2';
+let b = '3';
+alert(+a + +b);
