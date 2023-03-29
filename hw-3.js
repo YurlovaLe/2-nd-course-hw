ab9ea3bb6f140da0871d9a6f7615cabb112c1e77
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-03-29 11:26:07 +0700

    task1

---
+++ hw-3.js
@@ -0,0 +1,9 @@
+//Задание 1
+let password = 'пароль';
+let enteredPassword = prompt('Введите пароль');
+if (enteredPassword===password) {
+  console.log('Пароль введен верно');
+} else {
+  console.log('Пароль введен неправильно');
+}
+
