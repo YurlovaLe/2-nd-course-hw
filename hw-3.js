da3f0cbaac80b5e2e5cced53cd6adafa75fcbff3
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-03-29 12:13:39 +0700

    task5

--- hw-3.js
+++ hw-3.js
@@ -49,3 +49,35 @@ if (d>100 || e>100) {
 let a = '2';
 let b = '3';
 alert(+a + +b);
+
+//Задание 5
+let monthNumber = +prompt('Введите номер месяца от 1 до 12');
+switch (monthNumber) {
+  case 12:
+  case 1:
+  case 2:
+    console.log('Зима');
+    break;
+
+  case 3:
+  case 4:
+  case 5:
+    console.log('Весна');
+    break;
+
+  case 6:
+  case 7:
+  case 8:
+     console.log('Лето');
+     break;
+
+  case 9:
+  case 10:
+  case 11:
+     console.log('Осень');
+     break;
+
+  default:
+    console.log('Номер месяца должен быть от 1 до 12');
+    break;
+}
