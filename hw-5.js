dcbe72b5456f2665bc0585b42d63c7d78da4e9e9
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-09 20:24:16 +0700

    task3

--- hw-5.js
+++ hw-5.js
@@ -14,10 +14,27 @@ console.log(higherNumber(6, 6));
 //Задание 2
 
 let n = prompt("Введи число")
+
 function evenCheck() {
   let t = (n % 2 ==0) ? 'Число четное' : 'Число нечетное';
   return t;
 }
 
 evenCheck();
+
+//Задание 3.1
+
+function squaring(d) {
+  console.log(d * d);
+}
+
+squaring(8);
+
+//Задание 3.2
+
+function squaring(d) {
+  return(d * d);
+}
+
+squaring(8);
 
