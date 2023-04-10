a9a72a6e56a0ffbe6e6a238ba970cf8a9389a6da
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-10 13:27:02 +0700

    change function saesonsGame

--- site/index.js
+++ site/index.js
@@ -1,31 +1,14 @@
 function seasonsGame(monthNumber = Number(prompt("Введи номер месяца от 1 до 12"))) {
-  switch (monthNumber) {
-    case 12:
-    case 1:
-    case 2:
-      console.log('Зима');
-      break;
-  
-    case 3:
-    case 4:
-    case 5:
-      console.log('Весна');
-      break;
-  
-    case 6:
-    case 7:
-    case 8:
-       console.log('Лето');
-       break;
-  
-    case 9:
-    case 10:
-    case 11:
-       console.log('Осень');
-       break;
-  
-    default:
-      console.log('Номер месяца должен быть от 1 до 12');
-      break;
-  }  
+
+  if (monthNumber === 1  || monthNumber === 2 || monthNumber === 12) {
+    console.log('Зима');
+  } else if (monthNumber === 3  || monthNumber === 4 || monthNumber === 5) {
+    console.log('Весна');
+  } else if (monthNumber === 6  || monthNumber === 7 || monthNumber === 9) {
+  console.log('Лето');
+  } else if (isNaN(monthNumber)) {
+    console.log('Месяц должен быть введен числом');
+  } else {
+  console.log('Номер месяца должен быть от 1 до 12');
+    }
 }
