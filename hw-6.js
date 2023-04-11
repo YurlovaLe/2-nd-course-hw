9dbaf7288a69178de604b8619b1e6d5fdc3b8adf
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-11 19:31:19 +0700

    task3

--- hw-6.js
+++ hw-6.js
@@ -17,3 +17,9 @@ const arr = [1, 5, 4, 10, 0, 3];
 let positionNumber = arr.indexOf(4) + 1;
 console.log(positionNumber);
 
+//Задание 3
+
+const a = [1, 3, 5, 10, 20];
+
+let b = a.join(' ');
+console.log(b);
