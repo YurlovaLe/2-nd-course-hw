91391701612c1a1b989d825f9b8c9db2fa0a0ce1
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-14 19:39:08 +0700

    task4

--- hw-7.js
+++ hw-7.js
@@ -3,3 +3,22 @@
 let str = 'js';
 str = str.toUpperCase();
 
+//Задание 2
+
+//Задание 3
+
+const a = 32.58884;
+
+console.log (Math.floor(a));
+console.log (Math.ceil(a));
+console.log (Math.round(a));
+
+//Задание 4
+
+let b = Math.max(52, 53, 49, 77, 21, 32);
+let c = Math.min(52, 53, 49, 77, 21, 32);
+
+console.log(`Наибольшее число ${b}`);
+console.log(`Наименьшее число ${c}`);
+
+
