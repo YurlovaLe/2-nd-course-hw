157ccfeb27e6d55a884c0d087778e0706d96626b
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-14 20:52:04 +0700

    task10

--- hw-7.js
+++ hw-7.js
@@ -53,3 +53,19 @@ let currentDate = new Date();
 let g = new Date(currentDate.setDate(currentDate.getDate() + 73));
 
 console.log(g);
+
+//Задание 10
+
+  function DateInfo(s) {
+    const days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+  "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    let myDate = new Date(s);
+    let fullDate = "Дата: " + myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() + " - это " + days[myDate.getDay()] + ".";
+    let fullTime = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+    console.log(fullDate);
+    console.log(fullTime);
+  }
+
+  DateInfo('1995-12-17T03:24:00');
+
