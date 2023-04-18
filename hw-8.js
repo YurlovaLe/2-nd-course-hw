d1497ab1c09aaf3e14d4f0a8681bb930ecc29b1c
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-04-18 11:55:08 +0700

    task2

--- hw-8.js
+++ hw-8.js
@@ -18,3 +18,21 @@ getResult ([3, 4, 1, 9], mult);
 getResult ([3, 4, 1, 9], sum);
 
 //Задание 2
+
+const users = [
+	{name: 'Jon', age: 22},
+	{name: 'Richard', age: 18},
+	{name: 'Anton', age: 32},
+	{name: 'Lida', age: 23},
+	{name: 'Bob', age: 44}
+];
+
+function compareUser(a, b) { 
+	if (a.age > b.age) return 1;
+	if (a.age < b.age) return -1;
+	return 0;
+}
+
+users.sort(compareUser);
+console.log(users);
+
